d0526a2d76cd6d5d67356af5b461a35c45ef025d
Abtahee Farhan Ador <abtahee.ador25@gmail.com>
2023-04-16 02:23:19 +0600

    user register graph added

--- Auth/Controllers/GraphDataController.cs
+++ Auth/Controllers/GraphDataController.cs
@@ -15,8 +15,8 @@ namespace Auth.Controllers
         }
 
         [HttpGet]
-        [Route("summary")]
-        public async Task<IActionResult> Get()
+        [Route("summary/med")]
+        public async Task<IActionResult> GetMed()
         {
             var diaries = _app.Diaries.ToList();
             GraphModel graph = new GraphModel();
@@ -75,6 +75,76 @@ namespace Auth.Controllers
                         error++;
                         break;
                 }
+            }
+            return Ok(graph);
+        }
+
+        [HttpGet]
+        [Route("summary/reg")]
+        public async Task<IActionResult> GetUser()
+        {
+            var users = _app.Patients.ToList();
+            GraphModel graph = new GraphModel();
+            int error = 0;
+            foreach (var user in users)
+            {
+                if (user.RegisterDate != null)
+                {
+                    var data = user.RegisterDate.Split('-')[1];
+
+                    switch (data)
+                    {
+                        case "01":
+                        case "1":
+                            graph.Jan++;
+                            break;
+                        case "02":
+                        case "2":
+                            graph.Feb++;
+                            break;
+                        case "03":
+                        case "3":
+                            graph.Mar++;
+                            break;
+                        case "04":
+                        case "4":
+                            graph.Apr++;
+                            break;
+                        case "05":
+                        case "5":
+                            graph.May++;
+                            break;
+                        case "06":
+                        case "6":
+                            graph.Jun++;
+                            break;
+                        case "07":
+                        case "7":
+                            graph.Jul++;
+                            break;
+                        case "08":
+                        case "8":
+                            graph.Aug++;
+                            break;
+                        case "09":
+                        case "9":
+                            graph.Sep++;
+                            break;
+                        case "10":
+                            graph.Oct++;
+                            break;
+                        case "11":
+                            graph.Nov++;
+                            break;
+                        case "12":
+                            graph.Dec++;
+                            break;
+                        default:
+                            error++;
+                            break;
+                    }
+                }
+                
             }
             return Ok(graph);
         }
---
+++ Auth/Migrations/20230415202007_MeetingMigration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Auth.Migrations
+{
+    public partial class MeetingMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RegisterDate",
+                table: "Patients",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Meetings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    MeetingUrl = table.Column<string>(type: "text", nullable: false),
+                    PatientName = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Meetings", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Meetings");
+
+            migrationBuilder.DropColumn(
+                name: "RegisterDate",
+                table: "Patients");
+        }
+    }
+}
